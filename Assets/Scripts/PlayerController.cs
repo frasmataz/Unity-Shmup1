091cecd82ebf5f733ed03a7abb98360e84b2f27e
Fraser Sharp <fraserrsharp@gmail.com>
2016-01-28 17:47:51 +0000

    Added ship up-down animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ public class PlayerController : MonoBehaviour
 {
 	public float shipSpeed;
 	public float upDownVeloctiy;
-
+	
+	private Vector2 previousShipVelocity;
 	private Vector2 shipVelocity;
 	// Use this for initialization
 	void Start ()
@@ -16,11 +17,24 @@ public class PlayerController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		previousShipVelocity = shipVelocity;
 		shipVelocity = new Vector2 ( 
 		    Input.GetAxis("Horizontal") * shipSpeed,
 			Input.GetAxis("Vertical") * shipSpeed );
 
 		upDownVeloctiy = shipVelocity.y;
+
+		if (shipVelocity.y > 0 && shipVelocity.y > previousShipVelocity.y) {
+			GetComponent<Animator> ().SetBool ("Goingup", true);
+			GetComponent<Animator> ().SetBool ("Goingdown", false);
+		} else if (shipVelocity.y > 0 && shipVelocity.y < previousShipVelocity.y)
+			GetComponent<Animator>().SetBool("Goingup",false);
+
+		if (shipVelocity.y < 0 && shipVelocity.y < previousShipVelocity.y) {
+			GetComponent<Animator> ().SetBool ("Goingdown", true);
+			GetComponent<Animator> ().SetBool ("Goingup", false);
+		} else if (shipVelocity.y < 0 && shipVelocity.y > previousShipVelocity.y)
+			GetComponent<Animator>().SetBool("Goingdown",false);
 
 		GetComponent<Rigidbody2D> ().velocity = shipVelocity;
 	}
